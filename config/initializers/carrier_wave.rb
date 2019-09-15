4404a67df5cbc76dc368fd3ac7a93d9eaedecf86
hinumao-sut991 <6446cbhr@jcom.home.ne.jp>
2019-09-15 06:22:07 +0000

    Change Heroku setting

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,10 +6,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => 'ap-northeast-1',     # 例: 'ap-northeast-1'
+      :region                => ENV['S3_BREGION']
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  'rubyonrailsnumao446'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
